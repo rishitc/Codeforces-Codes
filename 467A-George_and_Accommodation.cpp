a23728a0048780f99afbd81d70480b327c1d363d
Rishit Chaudhary <rishit.c@live.com>
2021-06-19 21:34:55 +0530

    Added solution for 467A - George and Accommodation

---
+++ 467A-George_and_Accommodation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    int p, q;
+    int count{0};
+    const int Space_for_George_and_Alex = 2;
+
+    std::cin >> n;
+
+    while (n--)
+    {
+        std::cin >> p >> q;
+
+        if (q - p >= Space_for_George_and_Alex)
+        {
+            ++count;
+        }
+    }
+
+    std::cout << count << "\n";
+}
